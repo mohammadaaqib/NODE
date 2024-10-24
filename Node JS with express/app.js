1fefe6cbd42ef2ddcea09604e253c78fb82c6f0a
Muhammad Aqib <mohammadaaqib77@gmail.com>
2024-10-24 18:12:57 +0500

    adding functionallity like get create delete put patch

--- Node JS with express/app.js
+++ Node JS with express/app.js
@@ -1,23 +1,114 @@
 //Import express
-const express=require('express');
-let app=express();
+const express = require("express");
+const fs = require("fs");
+let app = express();
 
-//Route
-app.get('/',(req,res)=>[
-  //  res.status(200).send('<h4>Hello from express</h4>')
-    res.status(200).json({message:"hello from server"})
+let movies = JSON.parse(fs.readFileSync("./data/movies.json"));
 
-])
+app.use(express.json());
+
+//GET api
+app.get("/api/v1/movies", (req, res) => {
+  res.status(200).json({
+    status: "Sucess",
+    count: movies.length,
+    data: {
+      movies: movies,
+    },
+  });
+});
+
+//Get movie with id
+app.get("/api/v1/movies/:id/:name?", (req, res) => {
+  // console.log(req.params);
+  const id = +req.params.id;
+  const resMovie = movies.find((obj) => obj.id == id);
+
+  if (resMovie) {
+    res.status(200).json({
+      status: "success",
+      data: {
+        movie: resMovie,
+      },
+    });
+  } else {
+    res.status(404).json({
+      status: "fail",
+      message: "move with that id is not found",
+    });
+  }
+});
+
+//Post Api
+app.post("/api/v1/movies", (req, res) => {
+  const newId = movies[movies.length - 1].id + 1;
+  const newMovie = Object.assign({ id: newId }, req.body);
+  movies.push(newMovie);
+  fs.writeFile("./data/movies.json", JSON.stringify(movies), (err) => {
+    res.status(201).json({
+      status: "sucess",
+      data: {
+        movie: newMovie,
+      },
+    });
+  });
+
+  //   res.send("created");
+});
+
+//Patch api
+app.patch("/api/v1/movies/:id",(req,res)=>{
+    const id=req.params.id*1;
+  const movieToUpdate=  movies.find((obj)=>obj.id===id);
+
+  if(!movieToUpdate){
+   return   res.status(404).json({
+        status: "fail",
+        message: "move with that id is not found",
+      });
+  }
+  
+  const movieIndex=movies.indexOf(movieToUpdate);
+  movies[movieIndex]=Object.assign(movieToUpdate,req.body);
+  fs.writeFile("./data/movies.json", JSON.stringify(movies), (err) => {
+    res.status(200).json({
+      status: "success",
+      data: {
+        movie: movieToUpdate,
+      },
+    });
+  });
 
 
-app.post('/',(req,res)=>{
+})
+// Detele api
+app.delete("/api/v1/movies/:id",(req,res)=>{
+    const id=+req.params.id;
+    const movieToDelete= movies.find((obj)=>obj.id===id);
+    if(!movieToDelete){
+        return   res.status(404).json({
+            status: "fail",
+            message: "move with that id is not found",
+          });
+    }
+    const movieIndex=movies.indexOf(movieToDelete);
+
+    movies.splice(movieIndex,1);
+    fs.writeFile("./data/movies.json", JSON.stringify(movies), (err) => {
+        res.status(204).json({
+          status: "success",
+          data: {
+            movie: null,
+          },
+        });
+      })
 
 })
 
 
 
-const port=3000;
-app.listen(port,()=>{
-    console.log('server is started');
-})
 
+const port = 3000;
+app.listen(port, () => {
+  console.log("server is started");
+});
