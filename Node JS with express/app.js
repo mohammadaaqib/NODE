2796b97eaaf97ec226612b9a5d6d8491519bcf3f
Muhammad Aqib <mohammadaaqib77@gmail.com>
2024-10-24 18:21:21 +0500

    refector some basic code

--- Node JS with express/app.js
+++ Node JS with express/app.js
@@ -7,8 +7,8 @@ let movies = JSON.parse(fs.readFileSync("./data/movies.json"));
 
 app.use(express.json());
 
-//GET api
-app.get("/api/v1/movies", (req, res) => {
+//Route handler Functions
+const getAllMovies = (req, res) => {
   res.status(200).json({
     status: "Sucess",
     count: movies.length,
@@ -16,10 +16,9 @@ app.get("/api/v1/movies", (req, res) => {
       movies: movies,
     },
   });
-});
+};
 
-//Get movie with id
-app.get("/api/v1/movies/:id/:name?", (req, res) => {
+const getMovie = (req, res) => {
   // console.log(req.params);
   const id = +req.params.id;
   const resMovie = movies.find((obj) => obj.id == id);
@@ -37,10 +36,9 @@ app.get("/api/v1/movies/:id/:name?", (req, res) => {
       message: "move with that id is not found",
     });
   }
-});
+};
 
-//Post Api
-app.post("/api/v1/movies", (req, res) => {
+const createMovie = (req, res) => {
   const newId = movies[movies.length - 1].id + 1;
   const newMovie = Object.assign({ id: newId }, req.body);
   movies.push(newMovie);
@@ -54,22 +52,21 @@ app.post("/api/v1/movies", (req, res) => {
   });
 
   //   res.send("created");
-});
+};
 
-//Patch api
-app.patch("/api/v1/movies/:id",(req,res)=>{
-    const id=req.params.id*1;
-  const movieToUpdate=  movies.find((obj)=>obj.id===id);
+const updateMovie = (req, res) => {
+  const id = req.params.id * 1;
+  const movieToUpdate = movies.find((obj) => obj.id === id);
 
-  if(!movieToUpdate){
-   return   res.status(404).json({
-        status: "fail",
-        message: "move with that id is not found",
-      });
+  if (!movieToUpdate) {
+    return res.status(404).json({
+      status: "fail",
+      message: "move with that id is not found",
+    });
   }
-  
-  const movieIndex=movies.indexOf(movieToUpdate);
-  movies[movieIndex]=Object.assign(movieToUpdate,req.body);
+
+  const movieIndex = movies.indexOf(movieToUpdate);
+  movies[movieIndex] = Object.assign(movieToUpdate, req.body);
   fs.writeFile("./data/movies.json", JSON.stringify(movies), (err) => {
     res.status(200).json({
       status: "success",
@@ -78,35 +75,48 @@ app.patch("/api/v1/movies/:id",(req,res)=>{
       },
     });
   });
+};
 
+const deleteMovie = (req, res) => {
+  const id = +req.params.id;
+  const movieToDelete = movies.find((obj) => obj.id === id);
+  if (!movieToDelete) {
+    return res.status(404).json({
+      status: "fail",
+      message: "move with that id is not found",
+    });
+  }
+  const movieIndex = movies.indexOf(movieToDelete);
 
-})
-// Detele api
-app.delete("/api/v1/movies/:id",(req,res)=>{
-    const id=+req.params.id;
-    const movieToDelete= movies.find((obj)=>obj.id===id);
-    if(!movieToDelete){
-        return   res.status(404).json({
-            status: "fail",
-            message: "move with that id is not found",
-          });
-    }
-    const movieIndex=movies.indexOf(movieToDelete);
+  movies.splice(movieIndex, 1);
+  fs.writeFile("./data/movies.json", JSON.stringify(movies), (err) => {
+    res.status(204).json({
+      status: "success",
+      data: {
+        movie: null,
+      },
+    });
+  });
+};
 
-    movies.splice(movieIndex,1);
-    fs.writeFile("./data/movies.json", JSON.stringify(movies), (err) => {
-        res.status(204).json({
-          status: "success",
-          data: {
-            movie: null,
-          },
-        });
-      })
+// //GET api
+// app.get("/api/v1/movies", getAllMovies);
+// //Get movie with id
+// app.get("/api/v1/movies/:id", getMovie);
+// //Post Api
+// app.post("/api/v1/movies", createMovie);
+// //Patch api
+// app.patch("/api/v1/movies/:id", updateMovie);
+// // Detele api
+// app.delete("/api/v1/movies/:id", deleteMovie);
 
-})
+app.route("/api/v1/movies").get(getAllMovies).post(createMovie);
 
-
-
+app
+  .route("/api/v1/movies/:id")
+  .get(getMovie)
+  .patch(updateMovie)
+  .delete(deleteMovie);
 
 const port = 3000;
 app.listen(port, () => {
