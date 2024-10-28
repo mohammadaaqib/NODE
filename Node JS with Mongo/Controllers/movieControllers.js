01e3fd4e83aa82dd29d79edfb288b8641de11020
Muhammad Aqib <mohammadaaqib77@gmail.com>
2024-10-28 22:35:42 +0500

    import data and add sort limiting and paggincation

--- Node JS with Mongo/Controllers/movieControllers.js
+++ Node JS with Mongo/Controllers/movieControllers.js
@@ -1,9 +1,38 @@
 const Movie = require("./../Models/movieModel");
+const ApiFeatures=require("./../Utils/ApiFeatures")
 
 //Route handler Functions
 exports.getAllMovies = async (req, res) => {
+
   try {
-    const movies = await Movie.find();
+  const feature= new ApiFeatures(Movie.find(),req.query).sort().limiting().paggination();
+  let movies=await feature.query;
+  //  let query = Movie.find();
+
+    //sorting logic
+    // if (req.query.sort) {
+    //   const sortBy = req.query.sort.split(",").join(" ");
+    //   query = query.sort(sortBy);
+    // } else {
+    //   query = query.sort("-createdAt");
+    // }
+    //Limiting fields
+    // if (req.query.fields) {
+    //   console.log(req.query.fields);
+    //   const fields = req.query.fields.split(",").join(" ");
+    //   console.log(fields);
+    //   query = query.select(fields);
+    // } else {
+    //   query = query.select("-__v");
+    // }
+
+    //Pagination
+    // const page = req.query.page * 1 || 1;
+    // const limit = req.query.limit * 1 || 10;
+    // let skip = (page - 1) * limit;
+    // query = query.skip(skip).limit(limit);
+
+   // const movies = await query;
     res.status(200).json({
       status: "Success",
       lenght: movies.length,
@@ -60,10 +89,14 @@ exports.createMovie = async (req, res) => {
 
 exports.updateMovie = async (req, res) => {
   try {
-  const upadtedmovie= await  Movie.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const upadtedmovie = await Movie.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
 
     res.status(201).json({
       status: "Success",
@@ -72,7 +105,6 @@ exports.updateMovie = async (req, res) => {
       },
     });
   } catch (err) {
-
     res.status(400).json({
       status: "Fail",
       message: err.message,
@@ -82,19 +114,16 @@ exports.updateMovie = async (req, res) => {
 
 exports.deleteMovie = async (req, res) => {
   try {
-    const upadtedmovie= await  Movie.findByIdAndDelete(req.params.id);
-  
-      res.status(204).json({
-        status: "Success",
-        data: null
-      });
-    } catch (err) {
-  
-      res.status(400).json({
-        status: "Fail",
-        message: err.message,
-     
+    const upadtedmovie = await Movie.findByIdAndDelete(req.params.id);
 
-})
-}
+    res.status(204).json({
+      status: "Success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Fail",
+      message: err.message,
+    });
+  }
 };
--- Node JS with Mongo/Models/movieModel.js
+++ Node JS with Mongo/Models/movieModel.js
@@ -7,14 +7,63 @@ const movieschema = new mongoose.Schema({
       require: [true, "Name is required"],
       unique: true,
     },
-    duration: {
+    description: {
+      type: String,
+      required: [true, 'Description is required field!'],
+      trim: true
+  },
+  duration: {
       type: Number,
-      require: [true, "Duration is require field"],
-    },
-    description: String,
-    rating: Number,
-  });
-  
+      required: [true, 'Duration is required field!']
+  },
+  ratings: {
+      type: Number,
+      
+  },
+  totalRating: {
+      type: Number
+  },
+  releaseYear: {
+      type: Number,
+      required: [true, 'Release year is required field!']
+  },
+  releaseDate:{
+      type: Date
+  },
+  createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false
+  },
+  genres: {
+      type: [String],
+      required: [true, 'Genres is required field!'],
+      // enum: {
+      //      values: ["Action", "Adventure", "Sci-Fi", "Thriller", "Crime", "Drama", "Comedy", "Romance", "Biography"],
+      //      message: "This genre does not exist"
+      // }
+  },
+  directors: {
+      type: [String],
+      required: [true, 'Directors is required field!']
+  },
+  coverImage:{
+      type: String,
+      require: [true, 'Cover image is required field!']
+  },
+  actors: {
+      type: [String],
+      require: [true, 'actors is required field!']
+  },
+  price: {
+    type: Number,
+    require: [true, 'Price is required field!']
+},
+createdBy: String
+}, {
+toJSON: {virtuals: false},
+toObject: {virtuals: false}
+});
   const Movie = mongoose.model("Movie", movieschema);
 
   module.exports=Movie;
