fe2a6dbe95bbe008a2c453d9e8540c4243b4bda6
Muhammad Aqib <mohammadaaqib77@gmail.com>
2024-10-31 15:16:21 +0500

    creating user model

---
+++ Node JS with Mongo/Models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Enter name feild"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please Enter email."],
+    unique: true,
+    lowercase: true,
+    validator: [validator.isEmail, "Please enter a valid email"],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, "Please enter a password."],
+    minlength: 8,
+  },
+  consfirmpassword: {
+    type: String,
+    required: [true, "Please confirm your password"],
+  },
+});
+
+const User = mongooes.model("User", userSchema);
+module.exports = User;
